43e63e209074d307935b9d13b146caa9c35c0873
Travis Le <tle16@stanford.edu>
2015-02-19 19:21:01 -0800

    Added a dictionary class

---
+++ FE_Dict.py
@@ -0,0 +1,9 @@
+class FE_Dict(object):
+	"""docstring for FE_Dict"""
+	def __init__(self, arg):
+		super(FE_Dict, self).__init__()
+		self.dict = {
+			
+		}
+	def translate(self, word):
+		return self.dict[word]
