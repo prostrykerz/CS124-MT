cdf87c912b44a1f2a8429b8f3eb2c4541d1b0513
Raymond Chan <raymond.chan@DN51ro5l.SUNet>
2015-02-22 18:52:33 -0800

    Fixed FE_Dict class constructor and initializer

--- starter.py
+++ starter.py
@@ -1,5 +1,18 @@
+# coding: utf-8
+
+import io
+from FE_Dict import FE_Dict
+
 def main():
-	print "Hello World"
+    fe_dict = FE_Dict()
+
+    with io.open("sentences.txt", 'r', encoding="utf-8") as f:
+        for line in f:
+            line = line.replace(u'’', 'e ')
+            line = line.replace('\'', 'e ')
+
+            print fe_dict.translate("des")
+            print line
 
 if __name__ == '__main__':
 	main()
